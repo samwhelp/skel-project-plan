20a78888cf58eeb7061039121ddfd2d1b9a4586b
samwhelp <samwhelp@users.noreply.github.com>
2019-04-02 15:18:08 +0800

    echo source_file_path

--- example/project-vimrc/vimrc-static/build/bin/build.sh
+++ example/project-vimrc/vimrc-static/build/bin/build.sh
@@ -28,49 +28,75 @@ mvim_cat_conf_file () {
 	cat $file_path
 }
 
+mvim_apd_boot_file () {
+	local source_file_path="$THE_BOOT_DIR_PATH/$1"
+	local target_file_path="$2"
+	cat "$source_file_path" >> "$target_file_path"
+
+	echo
+	echo "   apd: $source_file_path"
+	echo "    to: $source_file_path"
+}
+
+mvim_apd_conf_file () {
+	local source_file_path="$THE_CONF_DIR_PATH/$1"
+	local target_file_path="$2"
+	cat "$source_file_path" >> "$target_file_path"
+
+	echo
+	echo "   apd: $source_file_path"
+	echo "    to: $source_file_path"
+}
+
+
 mvim_build_vimrc () {
 	local file_path="$THE_VIMRC_DIR_PATH/$1"
 
 	# head
+	
+	echo
+	echo "+Start: $file_path"
 	echo > "$file_path"
 
 	# boot
-	mvim_cat_boot_file "Start.vim" >> "$file_path"
+	mvim_apd_boot_file "Start.vim" "$file_path"
 
 
 	# conf
 	# conf - view
-	mvim_cat_conf_file "View/ColorTheme.vim" >> "$file_path"
-	mvim_cat_conf_file "View/VertSplit.vim" >> "$file_path"
-	mvim_cat_conf_file "View/StatusLine.vim" >> "$file_path"
-	mvim_cat_conf_file "View/TabLine.vim" >> "$file_path"
-	mvim_cat_conf_file "View/LineNumber.vim" >> "$file_path"
-	mvim_cat_conf_file "View/LineFormat.vim" >> "$file_path"
-	mvim_cat_conf_file "View/InvisibleChar.vim" >> "$file_path"
-	mvim_cat_conf_file "View/Folding.vim" >> "$file_path"
-	mvim_cat_conf_file "View/Scroll.vim" >> "$file_path"
-	mvim_cat_conf_file "View/Background.vim" >> "$file_path"
+	mvim_apd_conf_file "View/ColorTheme.vim" "$file_path"
+	mvim_apd_conf_file "View/VertSplit.vim" "$file_path"
+	mvim_apd_conf_file "View/StatusLine.vim" "$file_path"
+	mvim_apd_conf_file "View/TabLine.vim" "$file_path"
+	mvim_apd_conf_file "View/LineNumber.vim" "$file_path"
+	mvim_apd_conf_file "View/LineFormat.vim" "$file_path"
+	mvim_apd_conf_file "View/InvisibleChar.vim" "$file_path"
+	mvim_apd_conf_file "View/Folding.vim" "$file_path"
+	mvim_apd_conf_file "View/Scroll.vim" "$file_path"
+	mvim_apd_conf_file "View/Background.vim" "$file_path"
 
 
 	# conf - base
-	mvim_cat_conf_file "Base/Mark.vim" >> "$file_path"	
-	mvim_cat_conf_file "Base/Register.vim" >> "$file_path"
-	mvim_cat_conf_file "Base/Buffer.vim" >> "$file_path"
-	mvim_cat_conf_file "Base/Window.vim" >> "$file_path"
-	mvim_cat_conf_file "Base/TabPage.vim" >> "$file_path"
-	mvim_cat_conf_file "Base/Explore.vim" >> "$file_path"
-	mvim_cat_conf_file "Base/Mode.vim" >> "$file_path"
-	mvim_cat_conf_file "Base/Cache.vim" >> "$file_path"
+	mvim_apd_conf_file "Base/Mark.vim" "$file_path"	
+	mvim_apd_conf_file "Base/Register.vim" "$file_path"
+	mvim_apd_conf_file "Base/Buffer.vim" "$file_path"
+	mvim_apd_conf_file "Base/Window.vim" "$file_path"
+	mvim_apd_conf_file "Base/TabPage.vim" "$file_path"
+	mvim_apd_conf_file "Base/Explore.vim" "$file_path"
+	mvim_apd_conf_file "Base/Mode.vim" "$file_path"
+	mvim_apd_conf_file "Base/Cache.vim" "$file_path"
 
 
 	# conf - extra
-	mvim_cat_conf_file "Extra/Terminal.vim" >> "$file_path"
+	mvim_apd_conf_file "Extra/Terminal.vim" "$file_path"
 
 
 	# tail
 	echo >> "$file_path"
 
-	echo "Built: $file_path"
+	echo
+	echo "=Built: $file_path"
+
 }
 
 ##
@@ -84,7 +110,6 @@ mvim_build_vimrc () {
 
 __main__ () {
 	echo 'Building...'
-	echo
 	mvim_build_vimrc "vimrc"
 	mvim_build_vimrc "init.vim"
 	echo
