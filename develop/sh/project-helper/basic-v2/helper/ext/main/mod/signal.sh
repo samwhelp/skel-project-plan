3104400035fca7431da288a3e0fd1d54b127e2c4
samwhelp <samwhelp@users.noreply.github.com>
2025-05-25 14:00:14 +0800

    fix

--- develop/sh/project-helper/basic-v2/helper/ext/main/mod/signal.sh
+++ develop/sh/project-helper/basic-v2/helper/ext/main/mod/signal.sh
@@ -20,10 +20,28 @@ mod_action_on_signal_terminated () {
 
 }
 
+mod_action_on_err () {
+
+	sys_action_on_err
+
+	exit 0
+
+}
+
+mod_action_on_exit () {
+
+	sys_action_on_exit
+
+	exit 0
+
+}
+
 mod_main_signal_bind () {
 
 	trap mod_action_on_signal_interrupted SIGINT
 	trap mod_action_on_signal_terminated SIGTERM
+	trap mod_action_on_err ERR
+	#trap mod_action_on_exit EXIT
 
 }
 
