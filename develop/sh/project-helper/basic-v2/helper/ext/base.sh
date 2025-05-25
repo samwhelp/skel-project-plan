d0804f4b49a0624392d36f08da79471b6ab034e2
samwhelp <samwhelp@users.noreply.github.com>
2025-05-25 13:34:29 +0800

    fix

---
+++ develop/sh/project-helper/basic-v2/helper/ext/base.sh
@@ -0,0 +1,434 @@
+
+
+################################################################################
+### Head: Util / Debug
+##
+
+util_debug_echo () {
+
+	if is_debug; then
+		echo "${@}" 1>&2
+	fi
+
+}
+
+util_error_echo () {
+
+	echo "${@}" 1>&2
+
+}
+
+##
+### Head: Util / Debug
+################################################################################
+
+
+################################################################################
+### Head: Base
+##
+
+##
+## REF_BASE_DIR_PATH="$(cd -- "$(dirname -- "$0")" ; pwd)"
+##
+
+find_dir_path () {
+
+	if ! [ -d "$(dirname -- "${1}")" ]; then
+		dirname -- "${1}"
+		return 1
+	fi
+	echo "$(cd -- "$(dirname -- "${1}")" ; pwd)"
+
+}
+
+##
+## THIS_BASE_DIR_PATH="$(find_dir_path "${0}")"
+##
+
+
+##
+## $ export DEBUG_HELPER=true
+##
+
+is_debug () {
+
+	if [ "${DEBUG_HELPER}" = "true" ]; then
+		return 0
+	fi
+
+	return 1
+
+}
+
+is_not_debug () {
+
+	! is_debug
+
+}
+
+base_var_init () {
+
+	##
+	## ## Base Path
+	##
+
+	REF_INIT_DIR_PATH="$(find_dir_path "${REF_INIT_DIR_PATH}/.")"
+	REF_PLAN_DIR_PATH="$(find_dir_path "${REF_INIT_DIR_PATH}/../../.")"
+
+
+
+
+	##
+	## ## Main Path
+	##
+
+	REF_MAIN_DIR_NAME="helper"
+	REF_MAIN_DIR_PATH="${REF_PLAN_DIR_PATH}/${REF_MAIN_DIR_NAME}"
+
+
+	##
+	## ## Main Path / Sub
+	##
+
+	REF_MAIN_BIN_DIR_NAME="bin"
+	REF_MAIN_BIN_DIR_PATH="${REF_MAIN_DIR_PATH}/${REF_MAIN_BIN_DIR_NAME}"
+
+	REF_MAIN_EXT_DIR_NAME="ext"
+	REF_MAIN_EXT_DIR_PATH="${REF_MAIN_DIR_PATH}/${REF_MAIN_EXT_DIR_NAME}"
+
+	REF_MAIN_MAK_DIR_NAME="mak"
+	REF_MAIN_MAK_DIR_PATH="${REF_MAIN_DIR_PATH}/${REF_MAIN_MAK_DIR_NAME}"
+
+	REF_MAIN_SYS_DIR_NAME="sys"
+	REF_MAIN_SYS_DIR_PATH="${REF_MAIN_DIR_PATH}/${REF_MAIN_SYS_DIR_NAME}"
+
+	REF_MAIN_SHARE_DIR_NAME="share"
+	REF_MAIN_SHARE_DIR_PATH="${REF_MAIN_DIR_PATH}/${REF_MAIN_SHARE_DIR_NAME}"
+
+	REF_MAIN_TMP_DIR_NAME="tmp"
+	REF_MAIN_TMP_DIR_PATH="${REF_MAIN_DIR_PATH}/${REF_MAIN_TMP_DIR_NAME}"
+
+
+
+
+	##
+	## ## Main Path / Ext / Main
+	##
+
+	REF_MAIN_EXT_MAIN_DIR_NAME="main"
+	REF_MAIN_EXT_MAIN_DIR_PATH="${REF_MAIN_EXT_DIR_PATH}/${REF_MAIN_EXT_MAIN_DIR_NAME}"
+
+
+	##
+	## ## Main Path / Ext / Main / Sub
+	##
+
+	REF_MAIN_EXT_MAIN_UTIL_DIR_NAME="util"
+	REF_MAIN_EXT_MAIN_UTIL_DIR_PATH="${REF_MAIN_EXT_MAIN_DIR_PATH}/${REF_MAIN_EXT_MAIN_UTIL_DIR_NAME}"
+
+	REF_MAIN_EXT_MAIN_SYS_DIR_NAME="sys"
+	REF_MAIN_EXT_MAIN_SYS_DIR_PATH="${REF_MAIN_EXT_MAIN_DIR_PATH}/${REF_MAIN_EXT_MAIN_SYS_DIR_NAME}"
+
+	REF_MAIN_EXT_MAIN_MOD_DIR_NAME="mod"
+	REF_MAIN_EXT_MAIN_MOD_DIR_PATH="${REF_MAIN_EXT_MAIN_DIR_PATH}/${REF_MAIN_EXT_MAIN_MOD_DIR_NAME}"
+
+
+
+
+	##
+	## ## Main Path / Ext / Master
+	##
+
+	REF_MAIN_EXT_MASTER_DIR_NAME="master"
+	REF_MAIN_EXT_MASTER_DIR_PATH="${REF_MAIN_EXT_DIR_PATH}/${REF_MAIN_EXT_MASTER_DIR_NAME}"
+
+
+	##
+	## ## Main Path / Ext / Master / Sub
+	##
+
+	REF_MAIN_EXT_MASTER_UTIL_DIR_NAME="util"
+	REF_MAIN_EXT_MASTER_UTIL_DIR_PATH="${REF_MAIN_EXT_MASTER_DIR_PATH}/${REF_MAIN_EXT_MASTER_UTIL_DIR_NAME}"
+
+	REF_MAIN_EXT_MASTER_SYS_DIR_NAME="sys"
+	REF_MAIN_EXT_MASTER_SYS_DIR_PATH="${REF_MAIN_EXT_MASTER_DIR_PATH}/${REF_MAIN_EXT_MASTER_SYS_DIR_NAME}"
+
+	REF_MAIN_EXT_MASTER_MOD_DIR_NAME="mod"
+	REF_MAIN_EXT_MASTER_MOD_DIR_PATH="${REF_MAIN_EXT_MASTER_DIR_PATH}/${REF_MAIN_EXT_MASTER_MOD_DIR_NAME}"
+
+
+
+
+	##
+	############################################################################
+	##
+
+
+
+
+	##
+	## ## Master Path
+	##
+
+	REF_MASTER_DIR_PATH="${REF_PLAN_DIR_PATH}"
+
+
+	##
+	## ## Master Path / Sub
+	##
+
+	REF_MASTER_ASSET_DIR_NAME="asset"
+	REF_MASTER_ASSET_DIR_PATH="${REF_MASTER_DIR_PATH}/${REF_MASTER_ASSET_DIR_NAME}"
+
+	REF_MASTER_TOOL_DIR_NAME="tool"
+	REF_MASTER_TOOL_DIR_PATH="${REF_MASTER_DIR_PATH}/${REF_MASTER_TOOL_DIR_NAME}"
+
+
+	##
+	## ## Master Path / Asset / Sub
+	##
+
+	REF_MASTER_ASSET_OVERLAY_DIR_NAME="overlay"
+	REF_MASTER_ASSET_OVERLAY_DIR_PATH="${REF_MASTER_ASSET_DIR_PATH}/${REF_MASTER_ASSET_OVERLAY_DIR_NAME}"
+
+	REF_MASTER_ASSET_COMPONENT_DIR_NAME="component"
+	REF_MASTER_ASSET_COMPONENT_DIR_PATH="${REF_MASTER_ASSET_DIR_PATH}/${REF_MASTER_ASSET_COMPONENT_DIR_NAME}"
+
+	REF_MASTER_ASSET_PART_DIR_NAME="part"
+	REF_MASTER_ASSET_PART_DIR_PATH="${REF_MASTER_ASSET_DIR_PATH}/${REF_MASTER_ASSET_PART_DIR_NAME}"
+
+	REF_MASTER_ASSET_VENDOR_DIR_NAME="vendor"
+	REF_MASTER_ASSET_VENDOR_DIR_PATH="${REF_MASTER_ASSET_DIR_PATH}/${REF_MASTER_ASSET_VENDOR_DIR_NAME}"
+
+
+
+
+	return 0
+
+}
+
+base_var_dump () {
+
+	is_not_debug && return 0
+
+	util_debug_echo
+	util_debug_echo "################################################################################"
+	util_debug_echo "### Head: var_dump"
+	util_debug_echo "##"
+	util_debug_echo
+
+
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Note"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "##"
+	util_debug_echo "## **Hide This Info**"
+	util_debug_echo "##"
+	util_debug_echo "## \$ export DEBUG_HELPER=false"
+	util_debug_echo "##"
+	util_debug_echo
+
+
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Base Path"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "REF_BASE_DIR_PATH=${REF_BASE_DIR_PATH}"
+	util_debug_echo "REF_INIT_DIR_PATH=${REF_INIT_DIR_PATH}"
+	util_debug_echo "REF_PLAN_DIR_PATH=${REF_PLAN_DIR_PATH}"
+	util_debug_echo
+
+
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Main Path"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_DIR_NAME=${REF_MAIN_DIR_NAME}"
+	util_debug_echo "REF_MAIN_DIR_PATH=${REF_MAIN_DIR_PATH}"
+	util_debug_echo
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Main Path / Sub"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_BIN_DIR_NAME=${REF_MAIN_BIN_DIR_NAME}"
+	util_debug_echo "REF_MAIN_BIN_DIR_PATH=${REF_MAIN_BIN_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_EXT_DIR_NAME=${REF_MAIN_EXT_DIR_NAME}"
+	util_debug_echo "REF_MAIN_EXT_DIR_PATH=${REF_MAIN_EXT_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_MAK_DIR_NAME=${REF_MAIN_MAK_DIR_NAME}"
+	util_debug_echo "REF_MAIN_MAK_DIR_PATH=${REF_MAIN_MAK_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_SYS_DIR_NAME=${REF_MAIN_SYS_DIR_NAME}"
+	util_debug_echo "REF_MAIN_SYS_DIR_PATH=${REF_MAIN_SYS_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_SHARE_DIR_NAME=${REF_MAIN_SHARE_DIR_NAME}"
+	util_debug_echo "REF_MAIN_SHARE_DIR_PATH=${REF_MAIN_SHARE_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_TMP_DIR_NAME=${REF_MAIN_TMP_DIR_NAME}"
+	util_debug_echo "REF_MAIN_TMP_DIR_PATH=${REF_MAIN_TMP_DIR_PATH}"
+	util_debug_echo
+
+
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Main Path / Ext / Main"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_EXT_MAIN_DIR_NAME=${REF_MAIN_EXT_MAIN_DIR_NAME}"
+	util_debug_echo "REF_MAIN_EXT_MAIN_DIR_PATH=${REF_MAIN_EXT_MAIN_DIR_PATH}"
+	util_debug_echo
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Main Path / Ext / Main / Sub"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_EXT_MAIN_UTIL_DIR_NAME=${REF_MAIN_EXT_MAIN_UTIL_DIR_NAME}"
+	util_debug_echo "REF_MAIN_EXT_MAIN_UTIL_DIR_PATH=${REF_MAIN_EXT_MAIN_UTIL_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_EXT_MAIN_SYS_DIR_NAME=${REF_MAIN_EXT_MAIN_SYS_DIR_NAME}"
+	util_debug_echo "REF_MAIN_EXT_MAIN_SYS_DIR_PATH=${REF_MAIN_EXT_MAIN_SYS_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_EXT_MAIN_MOD_DIR_NAME=${REF_MAIN_EXT_MAIN_MOD_DIR_NAME}"
+	util_debug_echo "REF_MAIN_EXT_MAIN_MOD_DIR_PATH=${REF_MAIN_EXT_MAIN_MOD_DIR_PATH}"
+	util_debug_echo
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Main Path / Ext / Master"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_EXT_MASTER_DIR_NAME=${REF_MAIN_EXT_MASTER_DIR_NAME}"
+	util_debug_echo "REF_MAIN_EXT_MASTER_DIR_PATH=${REF_MAIN_EXT_MASTER_DIR_PATH}"
+	util_debug_echo
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Main Path / Ext / Master / Sub"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_EXT_MASTER_UTIL_DIR_NAME=${REF_MAIN_EXT_MASTER_UTIL_DIR_NAME}"
+	util_debug_echo "REF_MAIN_EXT_MASTER_UTIL_DIR_PATH=${REF_MAIN_EXT_MASTER_UTIL_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_EXT_MASTER_SYS_DIR_NAME=${REF_MAIN_EXT_MASTER_SYS_DIR_NAME}"
+	util_debug_echo "REF_MAIN_EXT_MASTER_SYS_DIR_PATH=${REF_MAIN_EXT_MASTER_SYS_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MAIN_EXT_MASTER_MOD_DIR_NAME=${REF_MAIN_EXT_MASTER_MOD_DIR_NAME}"
+	util_debug_echo "REF_MAIN_EXT_MASTER_MOD_DIR_PATH=${REF_MAIN_EXT_MASTER_MOD_DIR_PATH}"
+	util_debug_echo
+
+
+
+
+	##
+	############################################################################
+	##
+
+
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## ## Master Path"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "REF_MASTER_DIR_PATH=${REF_MASTER_DIR_PATH}"
+	util_debug_echo
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Master Path / Sub"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "REF_MASTER_ASSET_DIR_NAME=${REF_MASTER_ASSET_DIR_NAME}"
+	util_debug_echo "REF_MASTER_ASSET_DIR_PATH=${REF_MASTER_ASSET_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MASTER_TOOL_DIR_NAME=${REF_MASTER_TOOL_DIR_NAME}"
+	util_debug_echo "REF_MASTER_TOOL_DIR_PATH=${REF_MASTER_TOOL_DIR_PATH}"
+	util_debug_echo
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "## ## Master Path / Asset / Sub"
+	util_debug_echo "##"
+	util_debug_echo
+
+	util_debug_echo "REF_MASTER_ASSET_OVERLAY_DIR_NAME=${REF_MASTER_ASSET_OVERLAY_DIR_NAME}"
+	util_debug_echo "REF_MASTER_ASSET_OVERLAY_DIR_PATH=${REF_MASTER_ASSET_OVERLAY_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MASTER_ASSET_COMPONENT_DIR_NAME=${REF_MASTER_ASSET_COMPONENT_DIR_NAME}"
+	util_debug_echo "REF_MASTER_ASSET_COMPONENT_DIR_PATH=${REF_MASTER_ASSET_COMPONENT_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MASTER_ASSET_PART_DIR_NAME=${REF_MASTER_ASSET_PART_DIR_NAME}"
+	util_debug_echo "REF_MASTER_ASSET_PART_DIR_PATH=${REF_MASTER_ASSET_PART_DIR_PATH}"
+	util_debug_echo
+
+	util_debug_echo "REF_MASTER_ASSET_VENDOR_DIR_NAME=${REF_MASTER_ASSET_VENDOR_DIR_NAME}"
+	util_debug_echo "REF_MASTER_ASSET_VENDOR_DIR_PATH=${REF_MASTER_ASSET_VENDOR_DIR_PATH}"
+	util_debug_echo
+
+
+
+
+	util_debug_echo
+	util_debug_echo "##"
+	util_debug_echo "### Tail: var_dump"
+	util_debug_echo "################################################################################"
+	util_debug_echo
+
+
+	util_debug_echo
+	util_debug_echo
+	util_debug_echo
+
+
+	return 0
+
+}
+
+##
+### Tail: Base
+################################################################################
